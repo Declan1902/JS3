c83a3f46ba388f900884f9be72bbc050f3330168
Declan1902 <declansha@gmail.com>
2021-10-21 09:40:16 +0100

    Created odd or even (determined)

---
+++ script.js
@@ -0,0 +1,20 @@
+// let n=prompt("Enter your number")
+// if(n %2){
+//     //these numbers are odd
+//     alert("ODD")
+// }
+// else{
+//     alert("EVEN")
+
+// }
+
+    let n=prmompt("Enter your number")
+    if(n %1){
+        //these numbers are EVEN
+        alert("EVEN")
+    
+    }
+    else{
+        alert("ODD")
+    
+    }
